120e1e98bd945830a4a912aa3317659dad6bcc75
hi <hi@DESKTOP-S5GH85J>
2018-10-12 23:52:27 -0700

    test

--- src/main/java/utils/Reporter.java
+++ src/main/java/utils/Reporter.java
@@ -15,7 +15,7 @@ public abstract class Reporter {
 	public static ExtentReports extent;
 	public static ExtentTest test, suiteTest;
 	public String testCaseName, testNodes, testDescription, category, authors;
-	public String imagelocation="./../reports/images/";
+
 
 
 	public void startResult() {
@@ -50,7 +50,7 @@ public abstract class Reporter {
 			snapNumber = takeSnap();
 			try {
 				img = MediaEntityBuilder.createScreenCaptureFromPath
-						("./../reports/images/"+snapNumber+".jpg").build();
+						("./../reports/images/"+snapNumber+".jpeg;base64").build();
 			} catch (IOException e) {				
 			}
 		}
